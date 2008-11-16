b1ed8327914e8f908aa849c832ebb6e5a56d3896
rodrigooguerra <rodrigooguerra@yahoo.com.br>
2008-11-16 22:35:51 +0000

    Atualização

--- SGI/src/java/pgi/uniminas/acoes/ImovelAcao.java
+++ SGI/src/java/pgi/uniminas/acoes/ImovelAcao.java
@@ -19,8 +19,42 @@ public class ImovelAcao extends ActionSupport {
     private ImovelNeg imovelNeg = new ImovelNeg();
     private List<Imovel> listImovel;
 
+    public ImovelAcao() {
+    }
+
+    public ImovelNeg getImovelNeg() {
+        return imovelNeg;
+    }
+
+    public void setImovelNeg(ImovelNeg imovelNeg) {
+        this.imovelNeg = imovelNeg;
+    }
+
+    public List<Imovel> getListImovel() {
+        return listImovel;
+    }
+
+    public void setListImovel(List<Imovel> listImovel) {
+        this.listImovel = listImovel;
+    }
+
+    public String insertImovel(Imovel i){
+        imovelNeg.insertImovel(i);
+        return "Sucess";
+    }
+
     public String getImoveis() {
         listImovel = imovelNeg.getImoveis();
+        return "Sucess";
+    }
+
+    public String getImovel(int codImovel){
+        imovelNeg.getImovel(codImovel);
+        return "Sucess";
+    }
+
+    public String deleteImovel(Imovel [] i){
+        imovelNeg.deleteImovel(i);
         return "Sucess";
     }
 }
